e119dccbe5ef1925bc4c2c4a214336d6641fc811
JohannesWestberg <117350232+JohannesWestberg@users.noreply.github.com>
2024-01-26 00:32:44 +0100

    Add files via upload
    Product Page now working, however it is not responsive yet.

--- downTown/script.js
+++ downTown/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var hamburgerInput = document.querySelector('.hamburger-inp');
-    var hamburgerMenu = document.getElementById('hamburgerMenu');
+    let hamburgerInput = document.querySelector('.hamburger-inp');
+    let hamburgerMenu = document.getElementById('hamburgerMenu');
 
     hamburgerInput.addEventListener('click', function () {
         const show = document.getElementById( 'hamburgerMenu' );
@@ -19,6 +19,35 @@ document.addEventListener('DOMContentLoaded', function () {
             hide.style.display = "none"; // unset flex, so it returns to `none` as defined in the CSS.
         }
     });
-
-
 });
+
+
+// under this comment is the links between pages
+
+var logoDiv = document.querySelector('.logo');
+
+if (logoDiv) {
+    // Attach a click event listener to the div
+    logoDiv.addEventListener('click', function() {
+    // Perform the desired action when the div is clicked
+    var newPageURL = 'index.html';
+
+      // Open a new window or tab with the specified URL
+      window.open(newPageURL, 'index.html');
+    // You can replace the alert with any other action or function call
+  });
+}
+
+var vansDiv = document.querySelector('.vans');
+
+if (vansDiv) {
+    // Attach a click event listener to the div
+    vansDiv.addEventListener('click', function() {
+    // Perform the desired action when the div is clicked
+    var newPageURL = 'detail.html';
+
+      // Open a new window or tab with the specified URL
+      window.open(newPageURL, 'detail.html');
+    // You can replace the alert with any other action or function call
+  });
+}
